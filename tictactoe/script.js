c8e847098fc948942d3bc8a103898a4dd531a73e
Rylan Ho <170477157+rylanh1234@users.noreply.github.com>
2024-09-24 15:49:25 -0600

    Rearranged to play round upon click - player X starts first and turns alternate each round

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -5,7 +5,7 @@ const myGlobal = (function () {
     const playerTwoName = "Olive";
     const playerOne = createPlayerOne(playerOneName);
     const playerTwo = createPlayerTwo(playerTwoName);
-    return { gameOver, targetBlock, playerOne, playerTwo};
+    return { gameOver, targetBlock, playerOne, playerTwo };
 })();
 
 const gameboard = (function () {
@@ -22,23 +22,18 @@ const gameboard = (function () {
     return { blocktl, blocktc, blocktr, blockcl, blockc, blockcr, blockbl, blockbc, blockbr, blocks };
 })();
 
-function createPlayerOne (name) {
+function createPlayerOne(name) {
     const marker = "X";
-    return { name , marker};
+    return { name, marker };
 }
 
-function createPlayerTwo (name) {
+function createPlayerTwo(name) {
     const marker = "O";
-    return { name, marker};
+    return { name, marker };
 }
 
-function playRound(playerOne, playerTwo) {
-    if (gameboard[myGlobal.targetBlock] = "") {
-        gameboard[myGlobal.targetBlock] = myGlobal.playerOne.marker;
-    }
-    if (myGlobal.targetBlock = "") {
-        gameboard[myGlobaltargetBlock] = myGlobal.playerTwo.marker;
-    }
+function playRound(playerNum) {
+    gameboard[myGlobal.targetBlock] = playerNum.marker;
 }
 
 function gameOverCheck() {
@@ -71,22 +66,32 @@ function gameOverCheck() {
 const displayBoard = (function () {
     const boardContainer = document.querySelector(".boardContainer");
     gameboard.blocks.forEach((block, blockIndex) => {
-    const blockDiv = document.createElement("div");
-    blockDiv.classList.add("blockDiv");
-    blockDiv.setAttribute("id", Object.keys(gameboard)[blockIndex]);
-    boardContainer.appendChild(blockDiv);
+        const blockDiv = document.createElement("div");
+        blockDiv.classList.add("blockDiv");
+        blockDiv.setAttribute("id", Object.keys(gameboard)[blockIndex]);
+        boardContainer.appendChild(blockDiv);
     })
     const blockArray = document.querySelectorAll(".blockDiv");
+    let playerTurn = 0;
     blockArray.forEach((blockDiv) => {
-        blockDiv.addEventListener("click", function(e) {
+        blockDiv.addEventListener("click", function (e) {
             myGlobal.targetBlock = e.target.id;
+            if (gameboard[myGlobal.targetBlock] == "") {
+                if (playerTurn % 2 == 0) {
+                    playRound(myGlobal.playerOne);
+                    playerTurn += 1;
+                }
+                else {
+                    playRound(myGlobal.playerTwo);
+                    playerTurn += 1;
+                }
+            }
         })
     })
 })();
 
 const playGame = (function () {
     // while (myGlobal.gameOver == false) {
-        playRound(myGlobal.playerOne, myGlobal.playerTwo)
-        gameOverCheck();
+    gameOverCheck();
     // }
 })();

2dceb3250a47c17b460ec89d19a744bb3c4f25cd
Rylan Ho <170477157+rylanh1234@users.noreply.github.com>
2024-09-30 17:14:22 -0600

    Started restaurant page project Setup Webpack, gitignore, template html, and gathered assets

---
+++ restaurant-page/src/home.js
@@ -0,0 +1,6 @@
+const content = document.querySelector("#content");
+const back
+
+// <a href="https://www.flaticon.com/free-icons/bee" title="bee icons">Bee icons created by Smashicons - www.flaticon.com</a>
+// <a href="https://www.flaticon.com/free-icons/honeycomb" title="honeycomb icons">Honeycomb icons created by Freepik - www.flaticon.com</a>
+// <a href="https://www.freepik.com/free-vector/honey-comb-pattern_7997316.htm#fromView=search&page=1&position=10&uuid=6e0a4742-1530-4a88-aec9-5717a17f409f" title="honey comb patterns">Background image created by dgim-studio - www.freepik.com</a>

67c2cd5d07ac85f74920acfc6541bc9fcd32cb56
Rylan Ho <170477157+rylanh1234@users.noreply.github.com>
2024-10-04 17:22:04 -0600

    Added idea for the logic for buttons to switch tabs

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,6 +1,35 @@
 import "./styles.css";
 import { initPage } from "./home";
 
+const switchTab = (function () {
+    let currentPage = "home";
+    const tabBtns = document.querySelectorAll("button");
+    tabBtns.forEach((tabBtn) => {
+        tabBtn.addEventListener("click", function(e) {
+            if (e.target.id != currentPage) {
+                if (e.target.id == "home") {
+                    menuContainer.style.display = "none";
+                    contactContainer.style.display = "none";
+                    homeContainer.style.display = "grid";
+                    currentPage = "home";
+                }
+                else if (e.target.id == "menu") {
+                    homeContainer.style.display = "none";
+                    contactContainer.style.display = "none";
+                    menuContainer.style.display = "grid";
+                    currentPage = "grid";
+                }
+                else if (e.target.id == "contact") {
+                    homeContainer.style.display = "none";
+                    menuContainer.style.display = "none";
+                    contactContainer.style.display = "grid";
+                    currentPage = "contact";
+                }
+            }
+        })
+    })
+})();
+
 const loadRestaurantPage = (function() {
     initPage();
 }
